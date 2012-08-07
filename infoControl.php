ea8ec54c551c64fbada3674b58d34adb231c6519
Theodore <tnpham@ualberta.ca>
2012-08-07 15:56:38 -0600

    Added conditions for file retrieval

--- infoControl.php
+++ infoControl.php
@@ -38,78 +38,80 @@ require_once(dirname(__FILE__) . '/locallib.php');
 $user = optional_param('user',NULL, PARAM_TEXT);
 $pass = optional_param('pass',NULL, PARAM_TEXT);
 
-$download = optional_param('download',NULL, PARAM_TEXT);
+$downloadAll = optional_param('download',NULL, PARAM_TEXT);
+
+$file_to_DL = optional_param('filename', NULL, PARAM_TEXT);
 
 //echo $user;
 //echo $pass;
 	
-	if ((isset ($user)) && (isset ($pass)))
-	{
-            //Checks if user exists
-            $auth = authenticate_user_login($user, $pass);
-            //User exists
-            if($auth == true){
+if ((isset ($user)) && (isset ($pass)))
+{
+    //Checks if user exists
+    $auth = authenticate_user_login($user, $pass);
+    //User exists
+    if($auth == true){
 //                echo "true\n";
-              echo "true\n";
-             
-                $userinfo = $DB->get_record('user', array('username'=>$user));
-                
-                //Gets all the courses the user is enrolled in
-                $courses = enrol_get_users_courses($userinfo->id);
+        echo "true\n";
+
+        $userinfo = $DB->get_record('user', array('username'=>$user));
+
+        //Gets all the courses the user is enrolled in
+        $courses = enrol_get_users_courses($userinfo->id);
 //                print_object($courses);
-                $registered_courseids = array();
-                $registered_courseids = array_keys($courses);
-                //print_object($registered_courseids);
-                
-                $fs = get_file_storage();
-                
-                //Grabs the files
-                $filenames = $DB->get_records('qcardfiles', array('userid'=>$userinfo->id));
-                foreach ($filenames as $file){
-                    
-                    $courseid_array[] = $file->courseid;
-                    
-                    $coursename_array[] = $file->coursename;
-                    
-                    
-                    //echo $file->filename;
-                
-                //$file = $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'],
-                //$fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename'], $fileinfo['userid']);
-                $files = $fs->get_file($file->contextid, $file->component, $file->filearea,
-                $file->itemid, $file->filepath, $file->filename, $file->userid);
-                
-                //$files->readfile();             
-                }               
-                //Filters out the duplicate course ids and names and then reorders them
-                $conditions_list = array(TRUE, FALSE, NULL);
-                $unique_course_id = array_unique($courseid_array);
-                $ordered_course_id = reorderindex($unique_course_id, $conditions_list);    
+        $registered_courseids = array();
+        $registered_courseids = array_keys($courses);
+        //print_object($registered_courseids);
 
-                //print_object($ordered_course_id);
+        $fs = get_file_storage();
 
-                $unique_course_name = array_unique($coursename_array);
-                $ordered_course_name = reorderindex($unique_course_name, $conditions_list);
+        //Grabs the files
+        $filenames = $DB->get_records('qcardfiles', array('userid'=>$userinfo->id));
+        foreach ($filenames as $file){
 
-                //print_object($coursename_array);
-                //print_object($ordered_course_name);
-                //echo sizeof($ordered_course_name);
-                for($i=0; $i < sizeof($ordered_course_name); $i++){
-                    echo "course:\n";
-                    echo $ordered_course_name[$i];
+            $courseid_array[] = $file->courseid;
+
+            $coursename_array[] = $file->coursename;
+
+
+            //echo $file->filename;
+
+        //$file = $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'],
+        //$fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename'], $fileinfo['userid']);
+        $files = $fs->get_file($file->contextid, $file->component, $file->filearea,
+        $file->itemid, $file->filepath, $file->filename, $file->userid);
+
+        //$files->readfile();             
+        }               
+        //Filters out the duplicate course ids and names and then reorders them
+        $conditions_list = array(TRUE, FALSE, NULL);
+        $unique_course_id = array_unique($courseid_array);
+        $ordered_course_id = reorderindex($unique_course_id, $conditions_list);    
+
+        //print_object($ordered_course_id);
+
+        $unique_course_name = array_unique($coursename_array);
+        $ordered_course_name = reorderindex($unique_course_name, $conditions_list);
+
+        //print_object($coursename_array);
+        //print_object($ordered_course_name);
+        //echo sizeof($ordered_course_name);
+        for($i=0; $i < sizeof($ordered_course_name); $i++){
+            echo "course:\n";
+            echo $ordered_course_name[$i];
+            echo "\n";
+            //echo "<BR/>";
+
+            foreach ($filenames as $file){
+
+                if($file->coursename == $ordered_course_name[$i]){
+                    echo "file:\n";
+                    echo $file->filename;
                     echo "\n";
                     //echo "<BR/>";
-                    
-                    foreach ($filenames as $file){
-                        
-                        if($file->coursename == $ordered_course_name[$i]){
-                            echo "file:\n";
-                            echo $file->filename;
-                            echo "\n";
-                            //echo "<BR/>";
 
-                        }  
-                    }
+                }  
+            }
 //                    for($j=0; $j< sizeof($coursename_array); $j++){
 //                     
 //                        if($coursename_array[$j] == $ordered_course_name[$i]){
@@ -118,27 +120,36 @@ $download = optional_param('download',NULL, PARAM_TEXT);
 //                        
 //                    }
 
-                }
-                //Finds the courses that the teachers have uploaded files
-                if (sizeof($registered_courseids) > sizeof($ordered_course_id)) {
-                    $course_has_loader = array_intersect($registered_courseids, $ordered_course_id);
-                    $course_has_loader = reorderindex($course_has_loader);
-                } else {
-                    $course_has_loader = array_intersect($ordered_course_id, $registered_courseids);
-                    $course_has_loader = reorderindex($course_has_loader);
-                }
-
-                //print_object($course_has_loader);
-            //User !exists
-            } else if (user_exists($user) == false){
-                echo "DNE";
-            } else {
-                //Authetication failed
-                echo "false";
-            }
-	}      
-        if (isset($download)){
-           
-            echo "downloaded";        
         }
+        //Finds the courses that the teachers have uploaded files
+        if (sizeof($registered_courseids) > sizeof($ordered_course_id)) {
+            $course_has_loader = array_intersect($registered_courseids, $ordered_course_id);
+            $course_has_loader = reorderindex($course_has_loader);
+        } else {
+            $course_has_loader = array_intersect($ordered_course_id, $registered_courseids);
+            $course_has_loader = reorderindex($course_has_loader);
+        }
+
+        //print_object($course_has_loader);
+    //User !exists
+    } else if (user_exists($user) == false){
+        echo "DNE";
+    } else {
+        //Authetication failed
+        echo "false";
+    }
+} else if (isset($downloadAll)){
+
+    echo "downloaded";      
+    
+} else if (isset($file_to_DL)){
+    $fs = get_file_storage();
+    $file_property = $DB->get_record('qcardfiles', array('filename'=>$file_to_DL));
+
+    $file_content = $fs->get_file($file_property->contextid, $file_property->component, $file_property->filearea,
+    $file_property->itemid, $file_property->filepath, $file_property->filename, $file_property->userid);
+
+    $file_content->readfile();
+    return $file_content;
+}
 ?>
